382df1085928ebfb10dcc7dac79f5d969bece87c
yongchulbag <flypeace@naver.com>
2023-11-26 18:01:40 +0900

    이제시작

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Text(
-      'Home Screen',
-    )));
+      backgroundColor: Colors.pink[100],
+      body: SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              Text(
+                'U&I',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'parisienne',
+                    fontSize: 80),
+              ),
+              Text('우리 처음 만난날',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'sunflower',
+                      fontSize: 30)),
+              Text('2023. 11. 26',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'sunflower',
+                      fontSize: 20)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
+              Text('D+1',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'sunflower',
+                    fontSize: 50,
+                    fontWeight: FontWeight.w700,
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
