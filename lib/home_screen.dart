bff1785d9da2f0403a4482992c618793db66a87d
yongchulbag <flypeace@naver.com>
2023-11-26 20:48:36 +0900

    이제시작

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,39 +13,55 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Colors.pink[100],
       body: SafeArea(
+        bottom: false,
         child: Container(
           width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
-              Text(
-                'U&I',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'parisienne',
-                    fontSize: 80),
-              ),
-              Text('우리 처음 만난날',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'sunflower',
-                      fontSize: 30)),
-              Text('2023. 11. 26',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'sunflower',
-                      fontSize: 20)),
-              IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
-              Text('D+1',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'sunflower',
-                    fontSize: 50,
-                    fontWeight: FontWeight.w700,
-                  )),
+                  Expanded(child: _TopPart()),
+                  Expanded(child:Image.asset('asset/img/middle_image.png'),),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class _TopPart extends StatelessWidget {
+  const _TopPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(
+          'U&I',
+          style: TextStyle(
+              color: Colors.white, fontFamily: 'parisienne', fontSize: 80),
+        ),
+        Text('우리 처음 만난날',
+            style: TextStyle(
+                color: Colors.white, fontFamily: 'sunflower', fontSize: 30)),
+        Text('2023. 11. 26',
+            style: TextStyle(
+                color: Colors.white, fontFamily: 'sunflower', fontSize: 20)),
+        IconButton(
+            iconSize: 60,
+            onPressed: () {},
+            icon: Icon(
+              Icons.favorite,
+              color: Colors.red,
+            )),
+        Text('D+1',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'sunflower',
+              fontSize: 50,
+              fontWeight: FontWeight.w700,
+            )),
+      ],
     );
   }
 }
